c3a56baaaadddafe185bcad06c1458081f5a8ccb
HO VAN TAN <hvtan@dev.vn>
2020-12-05 09:26:19 +0700

    Release 1.0.0

---
+++ DawonHistoryMes/DawonHistoryMes/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace DawonHistoryMes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbDawonHistoryFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMesHistoryFolder = new System.Windows.Forms.TextBox();
+            this.nudCycletime = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDawon = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserMes = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudCycletime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbDawonHistoryFolder
+            // 
+            this.tbDawonHistoryFolder.Location = new System.Drawing.Point(130, 12);
+            this.tbDawonHistoryFolder.Name = "tbDawonHistoryFolder";
+            this.tbDawonHistoryFolder.Size = new System.Drawing.Size(360, 20);
+            this.tbDawonHistoryFolder.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dawon History Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mes History Folder";
+            // 
+            // tbMesHistoryFolder
+            // 
+            this.tbMesHistoryFolder.Location = new System.Drawing.Point(130, 38);
+            this.tbMesHistoryFolder.Name = "tbMesHistoryFolder";
+            this.tbMesHistoryFolder.Size = new System.Drawing.Size(360, 20);
+            this.tbMesHistoryFolder.TabIndex = 2;
+            // 
+            // nudCycletime
+            // 
+            this.nudCycletime.Location = new System.Drawing.Point(130, 64);
+            this.nudCycletime.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nudCycletime.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCycletime.Name = "nudCycletime";
+            this.nudCycletime.Size = new System.Drawing.Size(55, 20);
+            this.nudCycletime.TabIndex = 4;
+            this.nudCycletime.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCycletime.Click += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cycle Time";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Browser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(496, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Browser";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(496, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(415, 109);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Hide";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(191, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Min";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Dawon History";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 144);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudCycletime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbMesHistoryFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDawonHistoryFolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudCycletime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDawonHistoryFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMesHistoryFolder;
+        private System.Windows.Forms.NumericUpDown nudCycletime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDawon;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserMes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ DawonHistoryMes/DawonHistoryMes/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DawonHistoryMes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public string DPTpath, MESpath;
+        public DateTime now;
+        string buffer;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDawon.ShowDialog();
+            DPTpath = folderBrowserDawon.SelectedPath;
+            tbDawonHistoryFolder.Text = DPTpath;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserMes.ShowDialog();
+            MESpath = folderBrowserMes.SelectedPath;
+            tbMesHistoryFolder.Text = MESpath;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(@"C:\DEV_PJT\Dawon_MES_BRIDE\")) Directory.CreateDirectory(@"C:\DEV_PJT\Dawon_MES_BRIDE\");
+            string config = DPTpath + Environment.NewLine + MESpath + Environment.NewLine + nudCycletime.Value.ToString();
+            File.WriteAllText(@"C:\DEV_PJT\Dawon_MES_BRIDE\configs.cfg", config);
+
+            notifyIcon.Visible = true;
+            now = DateTime.Now;
+            string folderResult = now.ToString("yyyyMM");
+            tbDawonHistoryFolder.Text = DPTpath;
+            tbMesHistoryFolder.Text = MESpath;
+            timer1.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(@"C:\DEV_PJT\Dawon_MES_BRIDE\" + "buffer.txt"))
+                File.WriteAllText(@"C:\DEV_PJT\Dawon_MES_BRIDE\" + "buffer.txt", buffer);
+            else
+                buffer = File.ReadAllText(@"C:\DEV_PJT\Dawon_MES_BRIDE\" + "buffer.txt");
+
+            if (File.Exists(@"C:\DEV_PJT\Dawon_MES_BRIDE\" + "configs.cfg"))
+            {
+                string[] config = File.ReadAllLines(@"C:\DEV_PJT\Dawon_MES_BRIDE\" + "configs.cfg");
+                DPTpath = config[0];
+                MESpath = config[1];
+                nudCycletime.Value = Convert.ToInt32(config[2]);
+                timer1.Interval = (int)nudCycletime.Value * 1000 * 60;
+                tbDawonHistoryFolder.Text = DPTpath;
+                tbMesHistoryFolder.Text = MESpath;
+            }
+        }
+
+        public void copyFile()
+        {
+            now = DateTime.Now;
+            string folderResult = now.ToString("yy") + @"\" + now.ToString("MM");
+            string fileResult = now.ToString("dd");
+
+            if (!Directory.Exists(MESpath + @"\" + folderResult)) Directory.CreateDirectory(MESpath);
+
+            if (!File.Exists(MESpath + @"\" + fileResult + ".csv"))
+            {
+                if (Directory.Exists(DPTpath + @"\" + folderResult))
+                {
+                    Console.WriteLine(DPTpath + @"\" + folderResult);
+                    foreach (string f in Directory.GetFiles(DPTpath + @"\" + folderResult, "*.csv"))
+                    {
+                        if (f.Contains(fileResult))
+                        {
+                            Console.WriteLine(f);
+                            string extension = Path.GetExtension(f);
+                            if (extension != null && (extension.Equals(".csv")))
+                            {
+                                FileInfo fileInfo = new FileInfo(f);
+                                if (!IsFileLocked(fileInfo))
+                                {
+                                    string alldata = File.ReadAllText(f);
+                                    buffer = File.ReadAllText(@"C:\DEV_PJT\DPT_MES_BRIDE\" + "buffer.txt");
+                                    string newdata = "";
+                                    if (buffer.Length < alldata.Length)
+                                        newdata = alldata.Remove(0, buffer.Length);
+                                    else if (buffer.Length > alldata.Length)
+                                        newdata = alldata;
+                                    buffer = alldata;
+
+                                    if (newdata.Length > 0)
+                                        File.WriteAllText(MESpath + @"\" + fileInfo.Name, newdata);
+                                    File.WriteAllText(@"C:\DEV_PJT\DPT_MES_BRIDE\" + "buffer.txt", alldata);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)nudCycletime.Value * 1000 * 60;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            copyFile();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void notifyIcon_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        protected virtual bool IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+
+    }
+}
